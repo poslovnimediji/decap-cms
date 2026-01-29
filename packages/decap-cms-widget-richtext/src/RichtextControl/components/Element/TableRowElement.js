5feded19eea427287975052afe6400129c121ad0
Martin Jagodic <jagodicmartin1@gmail.com>
2026-01-29 11:18:51 +0100

    feat: add table preview in visual editor

---
+++ packages/decap-cms-widget-richtext/src/RichtextControl/components/Element/TableRowElement.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TableRowElement({ children, attributes, nodeProps }) {
+  return <tr {...attributes} {...nodeProps}>{children}</tr>;
+}
+
+export default TableRowElement;
